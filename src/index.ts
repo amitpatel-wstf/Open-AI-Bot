93d9137f8c06df462687ccdc6844b607eb7a8d7e
amitpatel-wstf <amit.patel@thewasserstoff.com>
2024-09-25 11:14:56 +0530

    changes in the gitignore

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,18 @@ const bot = new Bot(config.BOT_TOKEN); // <-- put your bot token between the ""
 createCommandMenu(bot);
 // Handle the /start command.
 
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+bot.command("start", (ctx) =>
+  ctx.reply(
+    `Welcome to Chat GPT - Turbo! 🚀
+
+I'm here to help you with anything—whether it's answering questions, brainstorming ideas, or just having a friendly chat. Ready to get started? Ask away, and I'll respond at turbo speed! ⚡
+
+Type your question or command below and let’s chat! 😊`,
+    {
+      parse_mode: "HTML",
+    }
+  )
+);
 bot.command("clearcontext", (ctx) => {
   BucketServices.clearBucket(ctx);
   ctx.reply("Context cleared..!");
@@ -24,10 +35,7 @@ bot.command("clearcontext", (ctx) => {
 // Handle other messages.
 bot.on("message", async (ctx) => {
   const message = ctx.update.message.text;
-  if (
-    message === "start" ||
-    message === "clearcontext"
-  ) {
+  if (message === "start" || message === "clearcontext") {
     return;
   }
   const messages = Bucket.updateBucket(ctx);
@@ -36,11 +44,10 @@ bot.on("message", async (ctx) => {
   ctx.reply(reponse);
 });
 
-app.listen(config.PORT,()=>{
+app.listen(config.PORT, () => {
   console.log(`Server is running on port ${config.PORT}`);
-})
+});
 
 bot.start().then(() => {
-  
   logger.info("Bot Server");
 });
